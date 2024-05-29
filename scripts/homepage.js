d127cb8eff91164c504a3d08307fd20f7fac2e7f
Notoric <tom@acornes.com>
2024-05-29 15:10:20 +0100

    Updated Aboutme and added scroll to top

--- scripts/homepage.js
+++ scripts/homepage.js
@@ -1,4 +1,5 @@
 const navbtn = document.getElementById('nav-btn');
+const topshrtct = document.getElementById('top-shrtct');
 
 navbtn.addEventListener('click', () => { 
   const navbar = document.getElementById('navbar');
@@ -6,3 +7,33 @@ navbtn.addEventListener('click', () => {
   navbtn.classList.toggle('expanded');
 });
 
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 100) {
+    topshrtct.classList.add('visible');
+  } else {
+    topshrtct.classList.remove('visible');
+  }
+});
+
+topshrtct.addEventListener('click', () => {
+  if (window.scrollY > 100) {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
+});
+
+async function updateAge() {
+  setInterval(() => {
+    const birthdate = new Date('2002-02-12T07:35:00Z');
+    let age = Date.now() - birthdate.getTime();
+    age = age / 31556952000;
+
+    age = age.toFixed(9);
+
+    const ageElement = document.getElementById('age');
+    ageElement.textContent = age;
+
+    console.log('Hello, world!');
+  }, 50);
+}
+
+updateAge();
